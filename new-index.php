cf27b55eb6a307da1864bca7b8118c66a85e6ed1
yukino1611 <paiman968546@gmail.com>
2026-04-03 10:25:13 +0200

    DENYS NAKALIAKAW
    SUKA DROCHKA Y 2.5 CHASA SYDYSH NIHUJA NE SDIELAW

---
+++ new-index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>new index</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Trispace:wght@100..800&display=swap" rel="stylesheet">
+    <style>
+       *{
+margin: 0;
+padding: 0;
+
+
+  font-family: "Trispace", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: <weight>;
+  font-style: normal;
+  font-variation-settings:
+    "wdth" 100;
+
+       } 
+       header{
+        display: flex;
+        width: 200vh;
+        height:100vh;
+        justify-content: center;
+        align-items :center;
+        background-color: black;
+       }
+       a{
+        padding: 50px;
+        border: 2px solid #ccff00;
+        text-decoration: none ;
+        color: #ccff00;
+       }
+       a:hover{
+        background-color: #ccff00;
+        color: black; 
+       }
+
+
+
+
+    </style>
+</head>
+<body>
+    <header>
+        <a href ="index.php">Cl1ck here if y0u n9ed swag</a>
+    </header>
+</body>
+</html>
